54fa53bc829fb39200307637e9863b924338aa1a
AkibRiad <akibriad652@gmail.com>
2024-10-13 21:25:50 +0600

    onk kicu add kora sesh

--- js/home.js
+++ js/home.js
@@ -1,13 +1,21 @@
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
-    console.log(1111);
+    const remove =document.getElementById('remove-hidden-cash');
+    remove.classList.add('hidden');
+    const addMoneyRemoved=document.getElementById('remove-hidden');
+    addMoneyRemoved.classList.remove('hidden');
 })
 
 
 // cashout section
 document.getElementById('cash-out').addEventListener('click',function(event){
     event.preventDefault();
-    console.log(1111);
+    const removed =document.getElementById('remove-hidden-cash');
+    removed.classList.remove('hidden');
+    const addMoneyRemoved=document.getElementById('remove-hidden');
+    addMoneyRemoved.classList.add('hidden');
+
+    
 })
 
 
@@ -28,9 +36,27 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
     else{
         alert('Wrong Pin');
     }
-
-    
-
-
-
 })
+
+    document.getElementById('cash-out-btn').addEventListener('click',function(event){
+        event.preventDefault();
+        const pinn = document.getElementById('pin-out').value;
+        if (pinn === '1123'){
+            const money = document.getElementById('available').innerText;
+            const enterMoney = document.getElementById('amount-out').value;
+            
+            const addNewMoney = parseInt(money);
+            const addedMoney = parseInt(enterMoney);
+            const newBalanace = addNewMoney - addedMoney;
+    
+            document.getElementById('available').innerText = newBalanace;
+            
+        }
+        else{
+            alert('Wrong Pin');
+        }
+
+    })
+
+
+
